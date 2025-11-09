ef237f6c9eeae2cdd7a7e94fc23cd7c7e7c4e09d
Igor Ryan <igorsonic56@gmail.com>
2025-11-09 23:20:15 +0530

    Adiicionando middlewares de autenticação e permissão

--- config/autenticacao.js
+++ config/autenticacao.js
@@ -1,40 +1,62 @@
-import passportLocal from "passport-local"
-import bcrypt from "bcryptjs"
-import db from "../models/index.js"
-const { Usuario } = db
+import passportLocal from "passport-local";
+import bcrypt from "bcryptjs";
+import db from "../models/index.js";
 
-const LocalStrategy = passportLocal.Strategy
+const { Usuario } = db;
+const LocalStrategy = passportLocal.Strategy;
 
-export default (passport) => {
+export default function inicializarPassport(passport) {
   passport.use(
     "usuario-local",
     new LocalStrategy(
       { usernameField: "email", passwordField: "senha" },
       async (email, senha, done) => {
         try {
-          const usuario = await Usuario.findOne({ where: { email } })
-          if (!usuario) return done(null, false, { message: "Usuário não encontrado" })
+          const usuario = await Usuario.findOne({ where: { email } });
+          if (!usuario) return done(null, false, { message: "Usuário não encontrado" });
 
-          const iguais = await bcrypt.compare(senha, usuario.senha)
-          if (!iguais) return done(null, false, { message: "Senha incorreta" })
+          const iguais = await bcrypt.compare(senha, usuario.senha);
+          if (!iguais) return done(null, false, { message: "Senha incorreta" });
 
-          return done(null, usuario)
+          return done(null, usuario);
         } catch (err) {
-          return done(err)
+          return done(err);
         }
       }
     )
-  )
+  );
 
-  passport.serializeUser((usuario, done) => done(null, usuario.id))
+  passport.serializeUser((usuario, done) => done(null, usuario.id));
 
   passport.deserializeUser(async (id, done) => {
     try {
-      const usuario = await Usuario.findByPk(id)
-      if (!usuario) return done(null, false)
-      return done(null, usuario)
+      const usuario = await Usuario.findByPk(id);
+      if (!usuario) return done(null, false);
+      return done(null, usuario);
     } catch (err) {
-      return done(err)
+      return done(err);
     }
-  })
+  });
+}
+
+// Middlewares de autenticação e permissão
+export function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.status(401).json({ message: "Usuário não autenticado." });
+}
+
+export function checkRole(role) {
+  return (req, res, next) => {
+    if (!req.isAuthenticated()) {
+      return res.status(401).json({ message: "Usuário não autenticado." });
+    }
+
+    if (req.user.tipo !== role) {
+      return res.status(403).json({ message: "Acesso negado." });
+    }
+
+    next();
+  };
 }
